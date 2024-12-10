acc302ce1cacc360322217dc6c7a6c3c8ff157be
mlnl <minicoz12@gmail.com>
2024-12-09 23:37:19 -0600

    adding in port checker

--- gluetun-auto-port-set/__init__.py
+++ gluetun-auto-port-set/__init__.py
@@ -22,22 +22,27 @@ class Plugin(BasePlugin):
         """This method will be called to perform the task and then reschedule itself."""
         self.update_port()
 
-        # Schedule the next run in 24 hours (86400 seconds)
-        threading.Timer(86400, self.run_update_port).start()
+        # Schedule the next run in 30 mins (1800 seconds)
+        threading.Timer(1800, self.run_update_port).start()
 
     def update_port(self):
-        try:
-            vpn_port = self.glutun_api.forwarded_port
-        except Exception as e:
-            self.log(f"Error fetching VPN port: {str(e)}")
-            return
-        try:
-            self.config.sections["server"]["portrange"] = (vpn_port, vpn_port)
-            self.core.reconnect()
-        except Exception as e:
-            self.log("Could not access settings or configuration. Required Nicotine version is 3.3.7+ for method reconnect")
-            self.log(e)
-            return
+        current_port = self.config.sections["server"]["portrange"][0]
+        if self.glutun_api.is_port_closed(port=current_port):
+            try:
+                self.log("Trying to get new port from gluetun")
+                vpn_port = self.glutun_api.forwarded_port
+            except Exception as e:
+                self.log(f"Error fetching VPN port: {str(e)}")
+                return
+            try:
+                self.config.sections["server"]["portrange"] = (vpn_port, vpn_port)
+                self.core.reconnect()
+            except Exception as e:
+                self.log("Could not access settings or configuration. Required Nicotine version is 3.3.7+ for method reconnect")
+                self.log(e)
+                return
+        else:
+            self.log("Port is already open.")
         
         new_port_range = self.config.sections["server"]["portrange"]
         self.log(f"New port range is: {new_port_range}, success")
